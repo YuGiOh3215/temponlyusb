7d9f4f15ca178d2c60235053aa36af9cf6e90618
ERoy <131496746+YuGiOh3215@users.noreply.github.com>
2023-06-23 15:53:12 -0400

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,164 @@
+#import weatherbit
+#import microbit
+
+#from microbit import *
+#from logging import *
+
+
+class TimeAndDate:
+#    Year = 2023
+#    Month = 5
+#    Day = 20
+    _refHours = 0
+    _refMinutes = 0
+    _refSeconds = 0
+#    Hours = 21
+#    Minutes = 20
+    Seconds = 0
+    _referenceCount = 0
+
+    def __init__(self):
+        self.Count = 0
+        self._referenceCount = 0
+
+    def start(self):
+        self._referenceCount = input.running_time()
+        self.Count = 0
+ 
+    def getTime(self):
+        self.Count = (input.running_time() - self._referenceCount)/1000
+        compare = self.Count - ((self._refHours * 3600) + \
+        (self._refMinutes * 60))
+
+        if (compare >= 60):
+            self.Seconds = Math.round_with_precision((compare - 60),0)
+            self._refMinutes = self._refMinutes + 1
+        else:
+            self.Seconds = Math.round_with_precision(compare,0)
+
+        if (self._refMinutes >= 60):
+            self._refMinutes = self._refMinutes - 60
+            self._refHours = self._refHours + 1
+
+        if (self._refHours >= 24):
+            self._refHours = self._refHours - 24
+
+        szLine = self.Count + "\t"+ self._refHours + \
+         ':' + self._refMinutes + ':' + self.Seconds
+        return szLine
+
+
+class LoggingParams:
+    idefaultLogInterv = 2000
+    _iLogInterval = 0
+    
+    def __init__(self):
+
+        self._iLogInterval = self.idefaultLogInterv
+
+    def getLogInterval(self):
+        return (self._iLogInterval)
+
+
+def showLoggingLED():
+    basic.show_leds("""
+        . . . . .
+        . . . . #
+        . . . # .
+        # . # . .
+        . # . . .
+    """)
+
+
+def showNotLoggingLED():
+    basic.show_leds("""
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+    """)
+
+def showQMarkLED():
+    basic.show_leds("""
+        . # # # .
+        . # # #.
+        . . # . .
+        . . . . .
+        . . # . .
+    """)
+
+   
+class dataOutput:
+    def __init__(self):
+        self.szLine = ""
+
+    def writeHeader(self):
+        self.szLine = 'Time\thh:mm:ss\tTiC'
+        serial.write_line(self.szLine)
+
+    def writeData(self,TTime, TiC):
+        self.szLine = TTime + '\t' + TiC
+
+        serial.write_line(self.szLine)
+
+
+def on_button_pressed_a():
+    global LoggingIsOn
+    LoggingIsOn = not (LoggingIsOn)
+    
+    if (LoggingIsOn == True):
+        showLoggingLED()
+    else:
+        showNotLoggingLED()
+        
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    
+    showQMarkLED()
+        
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+
+
+p1 = LoggingParams()
+dataLog = dataOutput()
+td = TimeAndDate()
+
+LoggingIsOn = False
+
+serial.redirect_to_usb()
+
+"""
+
+Note: If "???" is displayed, direction is unknown!
+
+"""
+
+
+def on_forever():
+    global p1, dataLog, td
+     
+    tempC = 0
+  
+    if LoggingIsOn == True:
+        # -------- wind --------
+
+        # -------- temperature --------
+        tempC = Math.round_with_precision(input.temperature(),2)
+        # -------- humidity --------
+
+        # -------- pressure --------
+
+        dataLog.writeData(td.getTime(), tempC)
+    else:
+        showNotLoggingLED()
+        
+    basic.pause(p1.getLogInterval())
+
+td.start()
+dataLog.writeHeader()
+while True:
+    on_forever()
+
---
+++ main.ts
@@ -0,0 +1,240 @@
+// import weatherbit
+// import microbit
+// from microbit import *
+// from logging import *
+class TimeAndDate {
+    private static _referenceCount: number
+    private ____referenceCount_is_set: boolean
+    private ____referenceCount: number
+    get _referenceCount(): number {
+        return this.____referenceCount_is_set ? this.____referenceCount : TimeAndDate._referenceCount
+    }
+    set _referenceCount(value: number) {
+        this.____referenceCount_is_set = true
+        this.____referenceCount = value
+    }
+    
+    private static _refHours: number
+    private ____refHours_is_set: boolean
+    private ____refHours: number
+    get _refHours(): number {
+        return this.____refHours_is_set ? this.____refHours : TimeAndDate._refHours
+    }
+    set _refHours(value: number) {
+        this.____refHours_is_set = true
+        this.____refHours = value
+    }
+    
+    private static _refMinutes: number
+    private ____refMinutes_is_set: boolean
+    private ____refMinutes: number
+    get _refMinutes(): number {
+        return this.____refMinutes_is_set ? this.____refMinutes : TimeAndDate._refMinutes
+    }
+    set _refMinutes(value: number) {
+        this.____refMinutes_is_set = true
+        this.____refMinutes = value
+    }
+    
+    static Seconds: number
+    private ___Seconds_is_set: boolean
+    private ___Seconds: number
+    get Seconds(): number {
+        return this.___Seconds_is_set ? this.___Seconds : TimeAndDate.Seconds
+    }
+    set Seconds(value: number) {
+        this.___Seconds_is_set = true
+        this.___Seconds = value
+    }
+    
+    private static _refSeconds: number
+    private ____refSeconds_is_set: boolean
+    private ____refSeconds: number
+    get _refSeconds(): number {
+        return this.____refSeconds_is_set ? this.____refSeconds : TimeAndDate._refSeconds
+    }
+    set _refSeconds(value: number) {
+        this.____refSeconds_is_set = true
+        this.____refSeconds = value
+    }
+    
+    Count: number
+    public static __initTimeAndDate() {
+        //     Year = 2023
+        //     Month = 5
+        //     Day = 20
+        TimeAndDate._refHours = 0
+        TimeAndDate._refMinutes = 0
+        TimeAndDate._refSeconds = 0
+        //     Hours = 21
+        //     Minutes = 20
+        TimeAndDate.Seconds = 0
+        TimeAndDate._referenceCount = 0
+    }
+    
+    constructor() {
+        this.Count = 0
+        this._referenceCount = 0
+    }
+    
+    public start() {
+        this._referenceCount = input.runningTime()
+        this.Count = 0
+    }
+    
+    public getTime() {
+        this.Count = (input.runningTime() - this._referenceCount) / 1000
+        let compare = this.Count - (this._refHours * 3600 + this._refMinutes * 60)
+        if (compare >= 60) {
+            this.Seconds = Math.roundWithPrecision(compare - 60, 0)
+            this._refMinutes = this._refMinutes + 1
+        } else {
+            this.Seconds = Math.roundWithPrecision(compare, 0)
+        }
+        
+        if (this._refMinutes >= 60) {
+            this._refMinutes = this._refMinutes - 60
+            this._refHours = this._refHours + 1
+        }
+        
+        if (this._refHours >= 24) {
+            this._refHours = this._refHours - 24
+        }
+        
+        let szLine = this.Count + "\t" + this._refHours + ":" + this._refMinutes + ":" + this.Seconds
+        return szLine
+    }
+    
+}
+
+TimeAndDate.__initTimeAndDate()
+
+class LoggingParams {
+    private static _iLogInterval: number
+    private ____iLogInterval_is_set: boolean
+    private ____iLogInterval: number
+    get _iLogInterval(): number {
+        return this.____iLogInterval_is_set ? this.____iLogInterval : LoggingParams._iLogInterval
+    }
+    set _iLogInterval(value: number) {
+        this.____iLogInterval_is_set = true
+        this.____iLogInterval = value
+    }
+    
+    static idefaultLogInterv: number
+    private ___idefaultLogInterv_is_set: boolean
+    private ___idefaultLogInterv: number
+    get idefaultLogInterv(): number {
+        return this.___idefaultLogInterv_is_set ? this.___idefaultLogInterv : LoggingParams.idefaultLogInterv
+    }
+    set idefaultLogInterv(value: number) {
+        this.___idefaultLogInterv_is_set = true
+        this.___idefaultLogInterv = value
+    }
+    
+    public static __initLoggingParams() {
+        LoggingParams.idefaultLogInterv = 2000
+        LoggingParams._iLogInterval = 0
+    }
+    
+    constructor() {
+        this._iLogInterval = this.idefaultLogInterv
+    }
+    
+    public getLogInterval(): number {
+        return this._iLogInterval
+    }
+    
+}
+
+LoggingParams.__initLoggingParams()
+
+function showLoggingLED() {
+    basic.showLeds(`
+        . . . . .
+        . . . . #
+        . . . # .
+        # . # . .
+        . # . . .
+    `)
+}
+
+function showNotLoggingLED() {
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+    `)
+}
+
+function showQMarkLED() {
+    basic.showLeds(`
+        . # # # .
+        . # # #.
+        . . # . .
+        . . . . .
+        . . # . .
+    `)
+}
+
+class dataOutput {
+    szLine: string
+    constructor() {
+        this.szLine = ""
+    }
+    
+    public writeHeader() {
+        this.szLine = "Time\thh:mm:ss\tTiC"
+        serial.writeLine(this.szLine)
+    }
+    
+    public writeData(TTime: any, TiC: number) {
+        this.szLine = TTime + "\t" + TiC
+        serial.writeLine(this.szLine)
+    }
+    
+}
+
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+    LoggingIsOn = !LoggingIsOn
+    if (LoggingIsOn == true) {
+        showLoggingLED()
+    } else {
+        showNotLoggingLED()
+    }
+    
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    showQMarkLED()
+})
+let p1 = new LoggingParams()
+let dataLog = new dataOutput()
+let td = new TimeAndDate()
+let LoggingIsOn = false
+serial.redirectToUSB()
+/** Note: If "???" is displayed, direction is unknown! */
+function on_forever() {
+    
+    let tempC = 0
+    if (LoggingIsOn == true) {
+        //  -------- wind --------
+        //  -------- temperature --------
+        tempC = Math.roundWithPrecision(input.temperature(), 2)
+        //  -------- humidity --------
+        //  -------- pressure --------
+        dataLog.writeData(td.getTime(), tempC)
+    } else {
+        showNotLoggingLED()
+    }
+    
+    basic.pause(p1.getLogInterval())
+}
+
+td.start()
+dataLog.writeHeader()
+while (true) {
+    on_forever()
+}
